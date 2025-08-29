56533b96cb44054fc7e235315b13401353fa1df4
Uzair Qamar <uzairqamarxyz@gmail.com>
2025-08-29 12:51:09 +0500

    Fix bandit error
    Signed-off-by: Uzair Qamar <uzairqamarxyz@gmail.com>

--- app/app.py
+++ app/app.py
@@ -53,4 +53,6 @@ def failcheck() -> Response:
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_RUN_PORT", "5000"))
+    app.run(host=host, port=port)
